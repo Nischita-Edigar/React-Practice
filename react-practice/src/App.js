4e99e2af0f44894916326dd63069f90164c23bfe
Nischita-Edigar <nischitav702@gmail.com>
2022-09-16 15:14:03 +0530

    Reactjs

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -2,14 +2,20 @@ import Login from './Components/functionalcmp/Login'
 import Footer from './Components/functionalcmp/Footer'
 import Example1 from './Components/classcmp/Example1'
 import Example2 from './Components/classcmp/Example2'
+import Empdata from './Display_data/Empdata'
+import User from './Display_data/User'
 
 let App=()=>{
     return <div>
         <h1>This is Functional component : </h1>
-    <Login/><Footer/>
+    {/* <Login/><Footer/> */}
     <h1>This is Class component : </h1>
-    <Example1/>
-    <Example2/>
+    {/* <Example1/>
+    <Example2/> */}
+    <hr>
+    </hr>  
+    <Empdata/>
+    <User/>
 
     </div>
 }
--- react-practice/src/Components/classcmp/Example1.js
+++ react-practice/src/Components/classcmp/Example1.js
@@ -1,6 +1,12 @@
 import React  from 'react'
 class Example1 extends React.Component{
+    constructor(props) {
+        super(props)
+        console.log("this is constructor")  //constructor method executes first
+        
+    }
     render(){
+        console.log("this is render")
         return <h2>Example 1 for class component</h2>
     }
 }
