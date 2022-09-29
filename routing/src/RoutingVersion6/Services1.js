f1fd8c586b1ab77bda216369ea18a28105a514cc
Nischita-Edigar <nischitav702@gmail.com>
2022-09-29 13:19:31 +0530

    Routing

---
+++ routing/src/RoutingVersion6/Services1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Services1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SERVICES PAGE</h1>
+      </div>
+    )
+  }
+}
+
+export default Services1
